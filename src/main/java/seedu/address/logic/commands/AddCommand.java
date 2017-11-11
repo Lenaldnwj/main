ff258c01aafb62915fd4f7a8588a9d0464bd339a
Lenald <lenaldnwj@gmail.com>
2017-11-12 00:42:59 +0800

    Improve invalid add message to inform users of which are the optional fields

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -24,28 +24,29 @@ public class AddCommand extends UndoableCommand {
     public static final String COMMAND_WORD = "add";
     public static final String COMMAND_ALIAS = "a";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
+    public static final String MESSAGE_USAGE = "Parameters enclosed in square brackets are optional.\n"
+            + COMMAND_WORD + ": Adds a person to the address book. "
             + "Parameters: "
             + PREFIX_NAME + "NAME "
-            + PREFIX_PHONE + "STUDENTPHONE "
+            + "[" + PREFIX_PHONE + "STUDENTPHONE] "
             + PREFIX_PARENTPHONE + "PARENTPHONE "
-            + PREFIX_EMAIL + "EMAIL "
-            + PREFIX_ADDRESS + "ADDRESS "
+            + "[" + PREFIX_EMAIL + "EMAIL] "
+            + "[" + PREFIX_ADDRESS + "ADDRESS] "
             + PREFIX_FORMCLASS + "FORMCLASS "
             + PREFIX_GRADES + "GRADES "
-            + PREFIX_POSTALCODE + "POSTALCODE "
+            + "[" + PREFIX_POSTALCODE + "POSTALCODE] "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
-            + PREFIX_PHONE + "97979797 "
+            + "[" + PREFIX_PHONE + "97979797] "
             + PREFIX_PARENTPHONE + "97303132 "
-            + PREFIX_EMAIL + "johnd@example.com "
-            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
+            + "[" + PREFIX_EMAIL + "johnd@example.com] "
+            + "[" + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25] "
             + PREFIX_FORMCLASS + "12S23 "
             + PREFIX_GRADES + "123.0 "
-            + PREFIX_POSTALCODE + "673349 "
-            + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + "[" + PREFIX_POSTALCODE + "673349] "
+            + "[" + PREFIX_TAG + "friends] "
+            + "[" + PREFIX_TAG + "owesMoney]";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
